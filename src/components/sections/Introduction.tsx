0ca1251b8f214b1c3e9a3a7cc4f3ac8be5812849
Abhinav pandey <pandeysandeep1190@gmail.com>
2022-07-04 00:33:31 -0700

    first commit

---
+++ src/components/sections/Introduction.tsx
@@ -0,0 +1,29 @@
+
+import { Link } from "react-scroll";
+
+export const Introduction = () => {
+	return (
+		<section id="introduction" className="introduction section is-medium">
+			<div className="introduction-container container">
+				<div className="columns">
+					<div className="column is-12">
+						
+							<div className="content" style={{ textAlign: "center" }}>
+								<h1 className="title">
+									Hello, I'm <span className="blue-text">Jan Leigh</span>.
+								</h1>
+								<p className="description">A Filipino full-stack web developer.</p>
+								<Link to="about" smooth={true} className="button floating">
+									View More
+									<span>→</span>
+								</Link>
+							</div>
+						
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Introduction;
